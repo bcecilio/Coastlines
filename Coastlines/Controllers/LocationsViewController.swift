bd0c62670f8b8d591d345258dcae2987fc16d101
brendon cecilio <brendon.cecilio@gmail.com>
2020-05-27 17:48:37 -0400

    extension for collectionView in locationsVC

--- Coastlines/Controllers/LocationsViewController.swift
+++ Coastlines/Controllers/LocationsViewController.swift
@@ -9,22 +9,37 @@
 import UIKit
 
 class LocationsViewController: UIViewController {
+    
+    private let locationsView = LocationsVew()
+    
+    override func loadView() {
+        view = locationsView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension LocationsViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        <#code#>
+    }
 }
