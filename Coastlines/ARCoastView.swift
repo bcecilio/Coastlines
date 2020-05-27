1d7996e541c39ddbed180a91edbf9f210cea0684
Ahad Islam <ahad.islam624@gmail.com>
2020-05-27 15:24:12 -0400

    created arcoastview, laid out arview

---
+++ Coastlines/ARCoastView.swift
@@ -0,0 +1,44 @@
+//
+//  ARCoastView.swift
+//  Coastlines
+//
+//  Created by Ahad Islam on 5/27/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ARCoastView: UIView {
+    
+    private lazy var arView = ARView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupView()
+    }
+    
+    
+    private func setupView() {
+        backgroundColor = .systemBlue
+        setupARView()
+    }
+    
+    private func setupARView() {
+        addSubview(arView)
+        arView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            arView.topAnchor.constraint(equalTo: topAnchor),
+            arView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            arView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            arView.bottomAnchor.constraint(equalTo:  bottomAnchor)])
+    }
+
+}
---
+++ Coastlines/ARViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ARViewController.swift
+//  Coastlines
+//
+//  Created by Ahad Islam on 5/27/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class ARViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
