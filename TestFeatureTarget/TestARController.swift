ae8235594b20bcde1abe89fd166dc916fbf51577
CameronRivera <cameronrivera@pursuit.org>
2020-06-09 15:16:53 -0400

    Added a second target for the purposes of testing AR.

---
+++ TestFeatureTarget/TestARController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TestFeatureTarget
+//
+//  Created by Cameron Rivera on 6/9/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class TestARController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
