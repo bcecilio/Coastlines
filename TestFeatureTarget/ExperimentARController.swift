370f3f2b38c7023198a394cb4370535173dc96e7
Kelby Mittan <kelbymittan@pursuit.org>
2020-06-11 16:19:27 -0400

    latest changes

--- TestFeatureTarget/ExperimentARController.swift
+++ TestFeatureTarget/ExperimentARController.swift
@@ -21,13 +21,13 @@ class ExperimentARController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-//        arView.session.delegate = self
+        arView.session.delegate = self
         
-//        setupARView()
+        setupARView()
+
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
 //
-//        arView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
-        
-//        let sceneAnchor = try!
+//        let sceneAnchor = try! CoastlinesNYC.loadScene1()
 //        arView.scene.addAnchor(sceneAnchor)
     }
     
@@ -45,47 +45,47 @@ class ExperimentARController: UIViewController {
     
     @objc
     func handleTap(recognizer: UITapGestureRecognizer) {
-//        let location = recognizer.location(in: arView)
-//
-//
-//        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
-//
-//        if let firstResult = results.first {
-//            let anchor = ARAnchor(name: "baseMapNYC", transform: firstResult.worldTransform)
-//
-//            arView.session.add(anchor: anchor)
-//
-//        } else {
-//            print("Object Placement Failed")
-//        }
+        let location = recognizer.location(in: arView)
+
+
+        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
+
+        if let firstResult = results.first {
+            let anchor = ARAnchor(name: "baseMapNYC-2", transform: firstResult.worldTransform)
+
+            arView.session.add(anchor: anchor)
+
+        } else {
+            print("Object Placement Failed")
+        }
         
 //        let sceneAnchor = try! FramedNYCAnimation.loadScene()
 //        arView.scene.addAnchor(sceneAnchor)
     }
     
-//    func placeObject(named entityName: String, for anchor: ARAnchor) {
-//        let entity = try! ModelEntity.loadModel(named: "baseMapNYC")
-//
-//
-//        entity.generateCollisionShapes(recursive: true)
-//
-//        arView.installGestures([.rotation, .translation], for: entity)
-//
-//        let anchorEntity = AnchorEntity(anchor: anchor)
-//
-//        anchorEntity.addChild(entity)
-//
-//        arView.scene.addAnchor(anchorEntity)
-//    }
+    func placeObject(named entityName: String, for anchor: ARAnchor) {
+        let entity = try! ModelEntity.loadModel(named: "baseMapNYC-2")
+
+
+        entity.generateCollisionShapes(recursive: true)
+
+        arView.installGestures([.rotation, .translation], for: entity)
+
+        let anchorEntity = AnchorEntity(anchor: anchor)
+
+        anchorEntity.addChild(entity)
+
+        arView.scene.addAnchor(anchorEntity)
+    }
 }
 
-//extension ExperimentARController: ARSessionDelegate {
-//
-//    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
-//        for anchor in anchors {
-//            if let anchorName = anchor.name, anchorName == "baseMapNYC" {
-//                placeObject(named: anchorName, for: anchor)
-//            }
-//        }
-//    }
-//}
+extension ExperimentARController: ARSessionDelegate {
+
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        for anchor in anchors {
+            if let anchorName = anchor.name, anchorName == "baseMapNYC-2" {
+                placeObject(named: anchorName, for: anchor)
+            }
+        }
+    }
+}
