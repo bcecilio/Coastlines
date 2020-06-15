0ee1b75d30fb005ecd125e035bc1434c7cf80074
Ahad Islam <ahad.islam624@gmail.com>
2020-06-15 10:44:46 -0400

    Pushed the thing

--- TestFeatureTarget/ExperimentARController.swift
+++ TestFeatureTarget/ExperimentARController.swift
@@ -12,7 +12,7 @@ import Combine
 
 class ExperimentARController: UIViewController {
     
-//    lazy var arView = ARView(frame: view.frame)
+    //    lazy var arView = ARView(frame: view.frame)
     
     let arView = ARView()
     
@@ -26,37 +26,26 @@ class ExperimentARController: UIViewController {
         
         setupARView()
         setupCoachingOverlayView()
+        
+        loadScene()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-//        arView.session.delegate = self
-//
-//        setupARView()
-//
-        arView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
-        
-//        let anchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.2, 0.2])
-//
-//        arView.scene.addAnchor(anchor)
-        
+    
+    private func addSpotlights() -> (SpotLight, SpotLight) {
         let sLight = SpotLight()
         let sLight2 = SpotLight()
         let sLight3 = SpotLight()
         
-        let testSlight = SpotLightComponent(color: .white, intensity: 500000, innerAngleInDegrees: 10, outerAngleInDegrees: 120, attenuationRadius: 20)
-        
-//        let boxMesh = MeshResource.generateBox(width: 72, height: 5, depth: 42)
-//        let material = SimpleMaterial()
-//        occBox = ModelEntity(mesh: boxMesh, materials: [material])
-//        occBox.position.y = 0.5
+        //        let boxMesh = MeshResource.generateBox(width: 72, height: 5, depth: 42)
+        //        let material = SimpleMaterial()
+        //        occBox = ModelEntity(mesh: boxMesh, materials: [material])
+        //        occBox.position.y = 0.5
         sLight.position =  [-0.2335, 0, 0.1584]
         sLight.light.attenuationRadius = 0.96
         sLight.light.color = .red
         sLight.light.intensity = 500000
-//        sLight.light.innerAngleInDegrees = 310
-//        sLight.light.outerAngleInDegrees = 135
+        //        sLight.light.innerAngleInDegrees = 310
+        //        sLight.light.outerAngleInDegrees = 135
         
         sLight.transform.rotation = simd_quatf(vector: [GLKMathDegreesToRadians(-60),GLKMathDegreesToRadians(0),GLKMathDegreesToRadians(90), 10])
         
@@ -65,36 +54,34 @@ class ExperimentARController: UIViewController {
         sLight2.light.color = .systemTeal
         sLight2.light.innerAngleInDegrees = 180
         sLight2.light.outerAngleInDegrees = 0
-//        sLight2.transform.rotation = simd_quatf(vector: [-30,0,90])
+        //        sLight2.transform.rotation = simd_quatf(vector: [-30,0,90])
         
         sLight3.position = [0.1644, 0.0487, 0.2044]
         sLight3.light.attenuationRadius = 0.16
         sLight3.light.color = .green
         sLight3.light.outerAngleInDegrees = 135
-//        sLight3.transform.rotation = simd_quatf(vector: [-30,0,90])
         
-        
-        do {
-            flipScene = try FlipRiseMap.loadFlipScene()
-            
-            arView.scene.anchors.append(flipScene)
-//            flipScene.addChild(occBox)
-            flipScene.addChild(sLight)
-//            flipScene.addChild(sLight2)
-            flipScene.addChild(sLight3)
-            
-            flipScene.components.set(testSlight)
-            
-        } catch {
-            print("error: \(error)")
-        }
-        
+        return (sLight, sLight3)
+    }
+    
+    private func addSpotlightComponent() -> SpotLightComponent {
+        return SpotLightComponent(color: .white, intensity: 500000, innerAngleInDegrees: 10, outerAngleInDegrees: 120, attenuationRadius: 20)
     }
     
     private func loadScene() {
-        FlipRiseMap.loadFlipSceneAsync { result in
+        FlipRiseMap.loadFlipSceneAsync { [unowned self] result in
             switch result {
+            case .failure(let error):
+                print(error)
+            case .success(let scene):
                 
+                let (spotlight1, spotlight2) = self.addSpotlights()
+                let spotlightComp = self.addSpotlightComponent()
+                
+                scene.addChild(spotlight1)
+                scene.addChild(spotlight2)
+                scene.components.set(spotlightComp)
+                self.arView.scene.anchors.append(scene)
             }
         }
     }
@@ -104,19 +91,19 @@ class ExperimentARController: UIViewController {
     func handleTap(recognizer: UITapGestureRecognizer) {
         
         flipScene.notifications.flipBehavior.post()
-//        let location = recognizer.location(in: arView)
-//
-//
-//        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
-//
-//        if let firstResult = results.first {
-//            let anchor = ARAnchor(name: "baseMapNYC-2", transform: firstResult.worldTransform)
-//
-//            arView.session.add(anchor: anchor)
-//
-//        } else {
-//            print("Object Placement Failed")
-//        }
+        //        let location = recognizer.location(in: arView)
+        //
+        //
+        //        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
+        //
+        //        if let firstResult = results.first {
+        //            let anchor = ARAnchor(name: "baseMapNYC-2", transform: firstResult.worldTransform)
+        //
+        //            arView.session.add(anchor: anchor)
+        //
+        //        } else {
+        //            print("Object Placement Failed")
+        //        }
         
         
         
@@ -124,16 +111,16 @@ class ExperimentARController: UIViewController {
     
     func placeObject(named entityName: String, for anchor: ARAnchor) {
         let entity = try! ModelEntity.loadModel(named: "baseMapNYC-2")
-
-
+        
+        
         entity.generateCollisionShapes(recursive: true)
-
+        
         arView.installGestures([.rotation, .translation], for: entity)
-
+        
         let anchorEntity = AnchorEntity(anchor: anchor)
-
+        
         anchorEntity.addChild(entity)
-
+        
         arView.scene.addAnchor(anchorEntity)
     }
     
@@ -151,7 +138,7 @@ class ExperimentARController: UIViewController {
 }
 
 extension ExperimentARController: ARSessionDelegate {
-
+    
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
         for anchor in anchors {
             if let anchorName = anchor.name, anchorName == "baseMapNYC-2" {
