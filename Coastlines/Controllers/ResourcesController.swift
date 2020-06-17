9c37e62c685e01cc437f9f2cfddd2f4b140ecf79
brendon cecilio <brendon.cecilio@gmail.com>
2020-06-17 16:19:57 -0400

    resize cells in resourceVC

--- Coastlines/Controllers/ResourcesController.swift
+++ Coastlines/Controllers/ResourcesController.swift
@@ -14,10 +14,10 @@ class ResourcesController: UIViewController {
     
     private var resources = [
         Resources(title: "Bike to Work!", description: "\u{2022} To help, we've rounded up our best tips for eco-friendly commuting, starting a carpool, commuting by bike, and much more in this guide. You'll save money, time, and reduce your carbon footprint -- and could even lose weight!"),
-        Resources(title: "Daily Actions You Can Take.", description: "bluhbluhbluh"),
+        Resources(title: "Daily Actions You Can Take.", description: "\u{2022} The number one goal? Limiting the use of fossil fuels such as oil, carbon and natural gas and replacing them with renewable and cleaner sources of energy. This includes driving and flying less, switching to a ‘green’ energy provider."),
         Resources(title: "Considering Solar Energy.", description: "bluhbluhbluh"),
-        Resources(title: "Can my diet make a difference?", description: "bluhbluhbluh"),
-        Resources(title: "Can't afford to cut down on the driving?", description: "bluhbluhbluh"),
+        Resources(title: "Can my diet make a difference?", description: "\u{2022} After fossil fuels, the food industry is one of the most important contributors to climate change. By reducing your consumption of animal protein by half you will cut your diet's carbon footprint by more than 40%."),
+        Resources(title: "Can't afford to cut down?", description: "bluhbluhbluh"),
         ]
     
     private var selectedIndex: IndexPath = IndexPath(row: 0, section: 0)
