3f42bee82606aaf98255f4ccc4e1ce2e155b0e72
brendon cecilio <brendon.cecilio@gmail.com>
2020-06-22 16:39:44 -0400

    added video file

--- Coastlines/Controllers/OnboardingController.swift
+++ Coastlines/Controllers/OnboardingController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class OnboardingController: UIViewController {
 
     private let secondOnboardingView: UIView
+    
+    private var player: AVPlayer?
     
     init(_ view: UIView){
         self.secondOnboardingView = view
@@ -32,6 +35,7 @@ class OnboardingController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        playBackgroundVideo()
         setUp()
     }
     
@@ -54,4 +58,21 @@ extension OnboardingController {
         }
     }
     
+    private func playBackgroundVideo() {
+        let path = Bundle.main.path(forResource: "IcebergPan", ofType: "mov")
+        player = AVPlayer(url: URL(fileURLWithPath: path!))
+        player!.actionAtItemEnd = AVPlayer.ActionAtItemEnd.none
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.frame
+        playerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        self.view.layer.insertSublayer(playerLayer, at: 0)
+        NotificationCenter.default.addObserver(self, selector: #selector(video), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: player!.currentItem)
+        player!.seek(to: CMTime.zero)
+        player!.play()
+        self.player?.isMuted = true
+    }
+    
+    @objc func video() {
+        player!.seek(to: CMTime.zero)
+    }
 }
