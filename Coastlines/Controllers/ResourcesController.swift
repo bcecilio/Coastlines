7c244757b9ca28a9708f4368558a53bea4bfdd2f
brendon cecilio <brendon.cecilio@gmail.com>
2020-06-23 06:30:04 -0400

    added more descriptions for resources cells

--- Coastlines/Controllers/ResourcesController.swift
+++ Coastlines/Controllers/ResourcesController.swift
@@ -15,9 +15,9 @@ class ResourcesController: UIViewController {
     private var resources = [
         Resources(title: "Bike to Work!", description: "\u{2022} To help, we've rounded up our best tips for eco-friendly commuting, starting a carpool, commuting by bike, and much more in this guide. You'll save money, time, and reduce your carbon footprint -- and could even lose weight!"),
         Resources(title: "Daily Actions You Can Take.", description: "\u{2022} The number one goal? Limiting the use of fossil fuels such as oil, carbon and natural gas and replacing them with renewable and cleaner sources of energy. This includes driving and flying less, switching to a ‘green’ energy provider."),
-        Resources(title: "Considering Solar Energy.", description: "bluhbluhbluh"),
+        Resources(title: "Considering Solar Energy.", description: "Renewables like wind and solar are becoming increasingly cheap across the world. The cost of utility-scale solar panels has fallen 73 percent since 2010, for example, making solar energy the cheapest source of electricity for many households in Latin America, Asia and Africa. In the UK, onshore wind and solar are competitive with gas and by 2025 will be the cheapest source of electricity generation."),
         Resources(title: "Can my diet make a difference?", description: "\u{2022} After fossil fuels, the food industry is one of the most important contributors to climate change. By reducing your consumption of animal protein by half you will cut your diet's carbon footprint by more than 40%."),
-        Resources(title: "Can't afford to cut down?", description: "bluhbluhbluh"),
+        Resources(title: "Can't afford to cut down?", description: "If you simply can’t make every change that’s needed, consider offsetting your emissions with a trusted green project – not a ‘get out of jail free card’, but another resource in your toolbox to compensate that unavoidable flight or car trip."),
         ]
     
     private var selectedIndex: IndexPath = IndexPath(row: 0, section: 0)
